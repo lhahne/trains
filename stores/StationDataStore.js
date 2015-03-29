4dc90b400ca8ce1cf6b07ce1539948ec488f5bc4
Lauri Hahne <lauri.hahne@leonidasoy.fi>
2015-03-29 14:12:51 +0300

    work on reactive store

--- stores/StationDataStore.js
+++ stores/StationDataStore.js
@@ -2,26 +2,64 @@
 
 var LiveDataActions = require('../actions/LiveDataActions')
 
-var _ = require('lodash-node')
+var _      = require('lodash-node')
+var Bacon  = require('baconjs')
+var moment = require('moment')
 
 var STATION_DATA_URL = 'http://rata.digitraffic.fi/api/v1/live-trains?station='
+var ACCEPTED_TRAINS  = ['IC', 'S', 'P']
+var TIME_FORMAT      = 'HH:mm'
 
-LiveDataActions.station.map((station) => {
-  console.log(station)
-  return STATION_DATA_URL + station
-})
+var formatTime = (time) =>
+  moment(time).format(TIME_FORMAT)
+
+var station = LiveDataActions.station
+
+var stationTrains = station
+  .map((station) =>
+    STATION_DATA_URL + station
+  )
+  .flatMap((url) =>
+    Bacon.fromPromise(fetch(url))
+  )
+  .flatMap((response) =>
+    Bacon.fromPromise(response.json())
+  )
+  .map((trains) =>
+    trains.filter((train) =>
+        _.contains(ACCEPTED_TRAINS, train.trainType))
+  )
+
+var stationArrivalView = stationTrains.zip(station, (st, s) => {
+    return {
+      station: s,
+      stationTrains: st
+    }
+  })
+  .map((stationData) => {
+    var station = stationData.station
+    var trains  = stationData.stationTrains
+
+    return _(trains).map((train) => {
+        var arrival = train.timeTableRows.filter((row) => {
+            return row.stationShortCode === station
+          })[0]
+
+        return {
+          trainNumber: train.trainType + ' ' + train.trainNumber,
+          arrivalTime: formatTime(arrival.scheduledTime),
+          actualArrivalTime: formatTime(arrival.liveEstimateTime),
+          late: arrival.differenceInMinutes > 0
+        }
+      })
+      .sortBy('arrivalTime')
+      .value()
+
+  })
+  .onValue((v) => console.log(v))
 
 
-// 
-// fetch('TPE')
-//   .then((response) => response.json())
-//   .then((responseData) => {
-//     var trains =_(responseData)
-//                   .filter((t) => _.contains(['IC', 'S', 'P'], t.trainType))
-//                   .value()
-//     this.setState({
-//       dataSource: this.state.dataSource.cloneWithRows(trains),
-//       loaded: true
-//     })
-//   })
-//   .done()
+module.exports = {
+  stationTrains: stationTrains,
+  //stationArrivalView: stationArrivalView
+}
